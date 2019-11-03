c2c1ca9596a179b49ebd2f87587589090f9e4d4f
elavarasanpl <sundarelavarasan@gmail.com>
2019-11-03 11:59:38 +0530

    git pages pushblishment

---
+++ build/precache-manifest.1a15805d14b4ea92975fd46789ac3c10.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "30fe813c54d564cc30236f1a4b396c71",
+    "url": "/library-app/static/media/item6.30fe813c.jpg"
+  },
+  {
+    "revision": "7a3fa9f0e51966a61cb12995e4200b94",
+    "url": "/library-app/static/media/item4.7a3fa9f0.jpg"
+  },
+  {
+    "revision": "6d1f914e8c7642053ca02957d1465fd6",
+    "url": "/library-app/static/media/item3.6d1f914e.jpg"
+  },
+  {
+    "revision": "5f88042d18f3e10c04c3e1610379307c",
+    "url": "/library-app/static/media/item1.5f88042d.jpg"
+  },
+  {
+    "revision": "d461c23b2f211bc191f1",
+    "url": "/library-app/static/js/runtime~main.d461c23b.js"
+  },
+  {
+    "revision": "62ea4933b177c0b2605a",
+    "url": "/library-app/static/js/main.62ea4933.chunk.js"
+  },
+  {
+    "revision": "30e578a2596d39c34d71",
+    "url": "/library-app/static/js/1.30e578a2.chunk.js"
+  },
+  {
+    "revision": "62ea4933b177c0b2605a",
+    "url": "/library-app/static/css/main.32bc343a.chunk.css"
+  },
+  {
+    "revision": "63127cfaa0a57436293e13c3755cdd1a",
+    "url": "/library-app/index.html"
+  }
+];
